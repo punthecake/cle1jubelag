c694d39cf5a8f205fce73ed59312d7319d729087
punthecake <60122576+punthecake@users.noreply.github.com>
2020-10-08 12:31:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -25,10 +25,8 @@
              </div>
             <div class="navmenu">
               <ul>
-                  <li><a class="active" href="#home">Home</a></li>
-                  <li><a href="#news">News</a></li>
-                  <li><a href="#contact">Contact</a></li>
-                  <li><a href="#about">About</a></li>
+                  <li><a class="active" href="#about">About</a></li>
+                  <li><a href="#product">Products</a></li>
               </ul>
             </div>
          </div>
@@ -39,11 +37,11 @@
                 <div class="half-cherry2"></div>
                 <div class="full-cherry"></div>
                 </div>
-         <div class="content">
+         <div class="content" id="about">
              <h1><center> In here, everything is delicious! </center></h1>
              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
              <h1><center> Products you might like: </center></h1>
-             <div class="product-container">
+             <div class="product-container" id="product">
                 <div class="product">
                     <div class="icecream"></div>
                     <div class="popsicle"></div>
